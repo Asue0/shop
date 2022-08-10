f6a9d36e60286516989dd21cddcf0c512222f7f6
jiho-kim <jh.kim@wyhil.com>
2022-08-10 17:56:50 +0900

    8월 10일자 업데이트 (미완성 코드 포함)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { Nav, Navbar, Container, Row, Col } from 'react-bootstrap';
 import './App.css';
+import bg from './img/bg.png';
+import data from './data.js';
+import { useState } from 'react';
 
 function App() {
+
+  let [shoes] = useState(data);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">쇼핑몰</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg" style={{ backgroundImage : 'url(' + bg + ')' }}></div>
+      <Container>
+        <Row>
+          {
+            shoes.map((a, i)=>{
+              return(
+                <Card shoes={shoes[i]} i={i}/>
+              )
+            })
+          }
+        </Row>
+      </Container>
     </div>
   );
+
+  function Card(props) {
+    return(
+        <Col sm>
+            <img src={'https://codingapple1.github.io/shop/shoes'+ (props.i+1) + '.jpg'} width="80%"/>
+            <h4>props.shoes.title </h4>
+            <p>props.shoes.price</p>
+        </Col>
+    )
+  }
+
 }
 
 export default App;
